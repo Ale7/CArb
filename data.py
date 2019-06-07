e1ddb6a2b72d24c573bd3a0e8f9847d427beca3c
Alec Wooding <alecwooding@gmail.com>
2019-06-06 23:34:50 -0600

    Improved appearance of data.py charts

--- data.py
+++ data.py
@@ -28,6 +28,7 @@ for r in result:
     charts[pair].get("x").append(time)
     charts[pair].get("y").append(spread)
 
+plt.style.use('dark_background')
 fig = plt.figure()
 date_fmt = '%H:%M'
 date_formatter = mdate.DateFormatter(date_fmt)
@@ -36,13 +37,17 @@ for i in range(1, 31):
     ax = fig.add_subplot(5, 6, i)
     pair = arb_pairs[i - 1]
     secs = mdate.epoch2num(charts[pair].get("x"))
-    ax.plot(secs, charts[pair].get("y"), color="green")
-    ax.set_title(pair, size=8)
+    ax.plot(secs, charts[pair].get("y"), color="#C0C0C0", linewidth=1)
+    ax.axhline(y=0.325, color='g', linestyle="dashed", linewidth=1)
+    ax.axhline(y=0, color='r', linewidth=1)
+    ax.set_title(pair, size=10)
+    ax.grid(color="#2F2F2F", linewidth=1)
+    ax.set_facecolor("#191919")
     ax.xaxis.set_major_formatter(date_formatter)
     ax.tick_params(axis='both', which='major', labelsize=6)
     ax.tick_params(axis='both', which='minor', labelsize=6)
 
-plt.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95, wspace=0.2, hspace=0.4)
+plt.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.95, wspace=0.2, hspace=0.4)
 plt.show()
 
 logging.info(timestamp() + f" - Finished running data.py")
