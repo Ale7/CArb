3b1cc76e74e56097d1035a2e926444afd2241f64
Alec Wooding <alecwooding@gmail.com>
2019-06-10 22:00:55 -0600

    Cleanup and created bot.py

--- simulate.py
+++ simulate.py
@@ -3,50 +3,14 @@ from research import *
 BOOK_DEPTH = 5
 PROFIT_PERCENT = 0.325
 
-
-def is_opportunity(low_book, high_book, req):
-    if low_book["asks"][0][0] * (100 + req) / 100 < high_book["bids"][0][0]:
-        return True
-    return False
-
-
-def find_quantity(book1, book2, floor):
-    qty = 0
-
-    if book1["bids"][0][0] > book2["asks"][0][0]:
-        low_book = book2
-        high_book = book1
-    elif book2["bids"][0][0] > book1["asks"][0][0]:
-        low_book = book1
-        high_book = book2
-    else:
-        return -1
-
-    while True:
-        if is_opportunity(low_book, high_book, floor):
-            if low_book["asks"][0][1] > high_book["bids"][0][1]:
-                q = high_book["bids"][0][1]
-                del high_book["bids"][0]
-                low_book["asks"][0][1] -= q
-            else:
-                q = low_book["asks"][0][1]
-                del low_book["asks"][0]
-                high_book["bids"][0][1] -= q
-            qty += q
-        else:
-            break
-
-    return qty
-
-
-simulate_pairs = ['ETH/BTC', 'RVN/BTC']
-
 logging.basicConfig(filename='SimulateLogging.log', level=logging.INFO)
 
 log("Started running simulate.py")
 
 binance = get_binance_connection(config.binance_api_key, config.binance_api_secret)
 bittrex = get_bittrex_connection(config.bittrex_api_key, config.bittrex_api_secret)
+
+simulate_pairs = ["ETH/BTC"]
 
 database = mysql_connect(config.mysql_host, config.mysql_username, config.mysql_password, config.mysql_db)
 mycursor = database.cursor()
