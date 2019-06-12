84de18b6f56f433bd4bb8e63f0f8f5b8e5b8b55d
Alec Wooding <alecwooding@gmail.com>
2019-06-12 11:01:44 -0600

    Progress on bot.py placing orders

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 from research import *
 import threading
 
-FREQUENCY = 15.0
-PROFIT = 2
+FREQUENCY = 60
+PROFIT = 0.375
 BOOK_DEPTH = 20
 
 
 def arbitrage():
-    # threading.Timer(FREQUENCY, arbitrage).start()
+    threading.Timer(FREQUENCY, arbitrage).start()
 
     # ex1_balances = get_nonzero_balances(exchanges[0].fetch_balance())
     # ex2_balances = get_nonzero_balances(exchanges[1].fetch_balance())
@@ -22,13 +22,27 @@ def arbitrage():
 
         book1 = exchanges[0].fetch_order_book(currency, BOOK_DEPTH)
         book2 = exchanges[1].fetch_order_book(currency, BOOK_DEPTH)
+        book_data = find_order_info(book1, book2, PROFIT)
 
-        x = find_order_info(book1, book2, PROFIT)
-
-        print(x)
-
-        # place limit buy order on LOW exchange: quantity = x[0], price = x[1]
-        # place limit sell order on HIGH exchange: quantity = x[0], price = x[2]
+        spread_info = find_spread(exchanges, currency)
+        low_exchange = spread_info[0]
+        high_exchange = spread_info[1]
+        if low_exchange.id == "bittrex":
+            symbol = currency
+            t = "limit"
+            side = "buy"
+            amount = book_data[0]
+            price = book_data[1]
+            params = {
+                "TimeInEffect": "IMMEDIATE_OR_CANCEL"
+            }
+            side_h = "sell"
+            price_h = book_data[2]
+            params_h = {
+                "timeInForce": "IOC"
+            }
+            low_exchange.create_order(symbol, t, side, amount, price, params)
+            high_exchange.create_order(symbol, t, side_h, amount, price_h, params_h)
 
 
 logging.basicConfig(filename='BotLogging.log', level=logging.INFO)
